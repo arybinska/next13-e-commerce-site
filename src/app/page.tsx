66053ed079b0eea5725214046ef2c49a5e173f27
arybinska <93257784+arybinska@users.noreply.github.com>
2023-09-12 21:34:33 +0200

    Creating products page for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,9 @@
-import { ProductList } from "../ui/organisms/ProductList";
-import { type ProductItemType } from "../ui/types";
-
-const products: ProductItemType[] = [
-	{
-		id: "1",
-		name: "Earthen Bottle",
-		category: "bottles",
-		price: 4800,
-		coverImage: {
-			src: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-			alt: "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-		},
-	},
-	{
-		id: "2",
-		name: "Nomad Tumbler",
-		category: "bottles",
-		price: 3500,
-		coverImage: {
-			src: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
-			alt: "Olive drab green insulated bottle with flared screw lid and flat top.",
-		},
-	},
-	{
-		id: "3",
-		name: "Focus Paper Refill",
-		category: "papers",
-		price: 8900,
-		coverImage: {
-			src: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
-			alt: "Person using a pen to cross a task off a productivity paper card.",
-		},
-	},
-	{
-		id: "4",
-		name: "Machined Mechanical Pencil",
-		category: "pencils",
-		price: 3500,
-		coverImage: {
-			src: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
-			alt: "Hand holding black machined steel mechanical pencil with brass tip and top.",
-		},
-	},
-];
+import Product from "./products/page";
 
 export default function Home() {
 	return (
-		<div className="bg-white">
-			<ProductList products={products} />
+		<div>
+			<Product />
 		</div>
 	);
 }
