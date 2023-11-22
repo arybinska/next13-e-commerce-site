28506e3bbf594285f22a553756f3617547564f74
arybinska <93257784+arybinska@users.noreply.github.com>
2023-11-23 00:27:00 +0100

    Week 4 - some fixes

--- src/app/@modal/(.)cart_NOTWORKING/page.tsx
+++ src/app/@modal/(.)cart_NOTWORKING/page.tsx
@@ -1,64 +1,64 @@
-// import { IncrementProductQuantity } from "../../../ui/atoms/IncrementProductQuantity";
-// import { getCartByFromCookies } from "../../../api/cart";
-// import { formatPrice } from "../../../utils";
-// import { CartModal } from "../../../ui/atoms/CartModal";
-// import { CartDeleteProductButton } from "../../../ui/atoms/CartDeleteRoductButton";
-// import { CartPaymentButon } from "../../../ui/atoms/CartPaymentButton";
+import { IncrementProductQuantity } from "../../../ui/atoms/IncrementProductQuantity";
+import { getCartByFromCookies } from "../../../api/cart";
+import { formatPrice } from "../../../utils";
+import { CartModal } from "../../../ui/atoms/CartModal";
+import { CartDeleteProductButton } from "../../../ui/atoms/CartDeleteRoductButton";
+import { CartPaymentButton } from "../../../ui/atoms/CartPaymentButton";
 
-// export default async function CartModalPage() {
-// 	const cart = await getCartByFromCookies();
-// 	if (!cart || cart.order?.orderItems.length === 0) {
-// 		return null;
-// 	}
+export default async function CartModalPage() {
+	const cart = await getCartByFromCookies();
+	if (!cart || cart.orderItems.length === 0) {
+		return null;
+	}
 
-// 	const total = cart.order?.orderItems.reduce(
-// 		(acc, item) =>
-// 			item.product ? acc + item.quantity * item.product.price : 0,
-// 		0,
-// 	);
+	const total = cart.orderItems.reduce(
+		(acc, item) =>
+			item.product ? acc + item.quantity * item.product.price : 0,
+		0,
+	);
 
-// 	return (
-// 		<CartModal>
-// 			<div className="p-6">
-// 				<h1 className="text-3xl">Order</h1>
-// 				<p>#{cart.order?.id}</p>
-// 				<ul className="flex w-full flex-col gap-5 py-6">
-// 					{cart.order?.orderItems.map(
-// 						(item) =>
-// 							item.product && (
-// 								<li key={item.product.id}>
-// 									<article className="grid grid-cols-5 items-center">
-// 										<span className="col-span-3">
-// 											{item.product.name}
-// 										</span>
-// 										<div className="col-span-1 self-center">
-// 											<IncrementProductQuantity
-// 												itemId={item.id}
-// 												quantity={item.quantity}
-// 												price={item.product.price}
-// 											/>
-// 										</div>
-// 										<span className="col-span-2 text-center">
-// 											{formatPrice(item.product.price / 100)}
-// 										</span>
-// 										<div className="col-span-1">
-// 											<CartDeleteProductButton itemId={item.id} />
-// 										</div>
-// 									</article>
-// 								</li>
-// 							),
-// 					)}
-// 					<li className="text-end text-2xl">
-// 						Total:{" "}
-// 						<span className="font-semibold">
-// 							{formatPrice(total / 100)}
-// 						</span>
-// 					</li>
-// 					<li>
-// 						<CartPaymentButon cart={cart} />
-// 					</li>
-// 				</ul>
-// 			</div>
-// 		</CartModal>
-// 	);
-// }
+	return (
+		<CartModal>
+			<div className="p-6">
+				<h1 className="text-3xl">Order</h1>
+				<p>#{cart.id}</p>
+				<ul className="flex w-full flex-col gap-5 py-6">
+					{cart.orderItems.map(
+						(item) =>
+							item.product && (
+								<li key={item.product.id}>
+									<article className="grid grid-cols-5 items-center">
+										<span className="col-span-3">
+											{item.product.name}
+										</span>
+										<div className="col-span-1 self-center">
+											<IncrementProductQuantity
+												itemId={item.id}
+												quantity={item.quantity}
+												price={item.product.price}
+											/>
+										</div>
+										<span className="col-span-2 text-center">
+											{formatPrice(item.product.price / 100)}
+										</span>
+										<div className="col-span-1">
+											<CartDeleteProductButton itemId={item.id} />
+										</div>
+									</article>
+								</li>
+							),
+					)}
+					<li className="text-end text-2xl">
+						Total:{" "}
+						<span className="font-semibold">
+							{formatPrice(total / 100)}
+						</span>
+					</li>
+					<li>
+						<CartPaymentButton />
+					</li>
+				</ul>
+			</div>
+		</CartModal>
+	);
+}
